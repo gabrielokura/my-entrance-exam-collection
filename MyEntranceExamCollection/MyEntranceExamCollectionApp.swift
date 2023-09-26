a40ca887fa40f0473d1ecaefc659ee6e840e6ac3
Gabriel Okura <gabriellevicz@gmail.com>
2023-09-26 09:22:51 -0300

    Initial Commit

---
+++ MyEntranceExamCollection/MyEntranceExamCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyEntranceExamCollectionApp.swift
+//  MyEntranceExamCollection
+//
+//  Created by Gabriel Motelevicz Okura on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MyEntranceExamCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
